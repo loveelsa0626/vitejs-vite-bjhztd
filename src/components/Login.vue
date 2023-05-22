89b0d0e0fe5725a66aa242535efbf8db625b5221
loveelsa0626 <879759871@qq.com>
2023-05-22 15:18:48 +0800

    5 files modified, 2 added, 2 deleted

---
+++ src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="home">login</div>
+</template>
+
+<script setup type="ts"></script>
+<style scoped>
+  #home {
+    width: 100%;
+    min-height: 100vh;
+    background: url("~@/images/home/h_bg.png") center center no-repeat;
+    background-size: 100% 100%;
+    
+  }
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path : "/",
+    redirect : {name : "login"}
+  },
+  {
+    name: 'login',
+    path: '/login',
+    component: () => import('@/components/Login.vue'),
+    meta: {
+      hidden: true,
+    },
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
